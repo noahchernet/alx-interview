636e94fff96a4249d681b7be0025372c106d0749
Noah Chernet <noahchernet@yahoo.com>
2022-06-21 17:22:14 +0300

    Initialized files for project 0x01

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,11 @@
+'''Module 0-Lockboxes
+Contains function canUnlockAll'''
+
+def canUnlockAll(boxes):
+  '''
+  Args:
+    boxes: (list) List of lists containing the indexes of the higher class
+    lists, acting as keys to them
+  Returns:
+    True if all boxes can be opened, False otherwise
+  '''
