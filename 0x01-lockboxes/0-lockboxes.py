50706b1e95a9b5573099c65b18250f3a97c2ff26
Noah Chernet <noahchernet@yahoo.com>
2022-06-21 23:39:24 +0300

    0-lockboxes.py: trying if decreasing indent passes checker

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,28 +7,25 @@ It checks if a list of boxes can be unlocked
 
 
 def canUnlockAll(boxes):
-    '''
-    boxes is a list of lists
-    A key with the same number as a box opens that box
-    You can assume all keys will be positive integers
-    There can be keys that do not have boxes
-    The first box boxes[0] is unlocked
-
-    Args:
-      boxes: (list) List of lists containing the indexes of the higher class
-      lists, acting as keys to them
-    Returns:
-      True if all boxes can be opened, False otherwise
-    '''
-    unlocked_boxes_indicies = [0]
-
-    for i in unlocked_boxes_indicies:
-        for j in boxes[i]:
-            if j not in unlocked_boxes_indicies:
-                unlocked_boxes_indicies.append(j)
-    unlocked_boxes_indicies = sorted(unlocked_boxes_indicies)
-    required_keys = [*range(1, len(boxes), 1)]
-
-    if set(required_keys) <= set(unlocked_boxes_indicies):
-        return True
-    return False
+  '''
+  boxes is a list of lists
+  A key with the same number as a box opens that box
+  You can assume all keys will be positive integers
+  There can be keys that do not have boxes
+  The first box boxes[0] is unlocked
+  Args:
+    boxes: (list) List of lists containing the indexes of the higher class
+    lists, acting as keys to them
+  Returns:
+    True if all boxes can be opened, False otherwise
+  '''
+  unlocked_boxes_indicies = [0]
+  for i in unlocked_boxes_indicies:
+      for j in boxes[i]:
+          if j not in unlocked_boxes_indicies:
+              unlocked_boxes_indicies.append(j)
+  unlocked_boxes_indicies = sorted(unlocked_boxes_indicies)
+  required_keys = [*range(1, len(boxes), 1)]
+  if set(required_keys) <= set(unlocked_boxes_indicies):
+      return True
+  return False
