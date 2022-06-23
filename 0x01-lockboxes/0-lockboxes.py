392e7130a5f6e5b34c3b49fb1363deb37b49393c
Noah Chernet <noahchernet@yahoo.com>
2022-06-23 23:55:45 +0300

    algorithm change to see if checker accepts

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,35 +1,32 @@
 #!/usr/bin/python3
-"""
-Module 0-Lockboxes
-Contains function canUnlockAll
-It checks if a list of boxes can be unlocked
-"""
+'''LockBoxes Challenge'''
 
 
 def canUnlockAll(boxes):
-    """
-    boxes is a list of lists
-    A key with the same number as a box opens that box
-    You can assume all keys will be positive integers
-    There can be keys that do not have boxes
-    The first box boxes[0] is unlocked
-
-    Args:
-      boxes (list): List of lists containing the indexes of the other class
-      lists, acting as keys to opening them
+    '''determines if all the boxes can be opened or not
     Returns:
-      True if all boxes can be opened, False otherwise
-    """
+        True: all boxes can be opened
+        False: not all boxes can be opened
+    '''
+    length = len(boxes)
+    keys = set()
+    opened_boxes = []
+    i = 0
 
-    unlocked_boxes_indicies = [0]
+    while i < length:
+        oldi = i
+        opened_boxes.append(i)
+        keys.update(boxes[i])
+        for key in keys:
+            if key != 0 and key < length and key not in opened_boxes:
+                i = key
+                break
+        if oldi != i:
+            continue
+        else:
+            break
 
-    for i in unlocked_boxes_indicies:
-        for j in boxes[i]:
-            if j not in unlocked_boxes_indicies:
-                unlocked_boxes_indicies.append(j)
-    unlocked_boxes_indicies = sorted(unlocked_boxes_indicies)
-    required_keys = [*range(1, len(boxes), 1)]
-
-    if set(required_keys) <= set(unlocked_boxes_indicies):
-        return True
-    return False
+    for i in range(length):
+        if i not in opened_boxes and i != 0:
+            return False
+    return True
