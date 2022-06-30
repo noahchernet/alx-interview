26f6fb4ef5d4655f66b33a2e3bf2e5011f986615
Noah Chernet <noahchernet@yahoo.com>
2022-06-30 20:08:30 +0300

    Done project 0x02

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+Module 0-minoperations.py
+Contains function minOperations(n)
+'''
+
+def minOperations(n):
+  '''
+  Calculates the fewest number of operations needed to result in exactly `n`
+  number of `H` characters in a file.
+  '''
+  ans = 0
+  d = 2
+  while n > 1:
+      while n % d == 0:
+          ans += d
+          n /= d
+      d += 1
+  return ans
