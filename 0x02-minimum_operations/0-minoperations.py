d91c568fc20cd9609a1e880773cef5ccc5994308
Noah Chernet <noahchernet@yahoo.com>
2022-06-30 20:13:24 +0300

    pep8 errors fixed

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,16 +4,17 @@ Module 0-minoperations.py
 Contains function minOperations(n)
 '''
 
+
 def minOperations(n):
-  '''
-  Calculates the fewest number of operations needed to result in exactly `n`
-  number of `H` characters in a file.
-  '''
-  ans = 0
-  d = 2
-  while n > 1:
-      while n % d == 0:
-          ans += d
-          n /= d
-      d += 1
-  return ans
+    '''
+    Calculates the fewest number of operations needed to result in exactly `n`
+    number of `H` characters in a file.
+    '''
+    ans = 0
+    d = 2
+    while n > 1:
+        while n % d == 0:
+            ans += d
+            n /= d
+        d += 1
+    return ans
