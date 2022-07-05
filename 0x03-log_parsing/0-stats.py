40101926b507a98937a08b256f0dc50b8225b6a3
Noah Chernet <noahchernet@yahoo.com>
2022-07-06 01:50:18 +0300

    Initialized files for project 0x03

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''Module 0-stats.py
+Reads standard input line by line and computes metrics:
+'''
+import fileinput
+
+
+def main():
+  '''Program execution starts here'''
+
+
+
+if __name__ == '__main__':
+  main()
