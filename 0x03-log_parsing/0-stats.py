78c455da32fd31b5e0a5a0ccea244519decadf72
Noah Chernet <noahchernet@yahoo.com>
2022-07-06 16:09:06 +0300

    pep8 error fixed

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -10,7 +10,7 @@ def main():
     count = 0
     total_file_size = 0
     status_codes = {200: 0, 301: 0,
-                                400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+                    400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
     try:
         '''Program execution starts here'''
