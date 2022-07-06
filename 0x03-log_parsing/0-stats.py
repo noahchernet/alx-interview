f5928f851abfa5b1b1301d11bbe04ea4b7a82362
Noah Chernet <noahchernet@yahoo.com>
2022-07-06 19:01:34 +0300

    pep8 error fixed

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
                 if statusC in statusC_counter:
                     statusC_counter[statusC] += 1
                 file_size = file_size + f_size
-            except:
+            except Exception as e:
                 pass
         printCodes(statusC_counter, file_size)
     except KeyboardInterrupt:
