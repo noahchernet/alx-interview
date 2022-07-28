ae46513e5091a2b6fd3234aa3f0dd06ed2311d6b
Noah Chernet <noahchernet@yahoo.com>
2022-07-28 18:55:17 +0300

    Done star wars task correctly

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,26 +2,40 @@
 // Prints all characters of a Star Wars movie, with the episode's number passed
 // as argument
 
-const request = require('request');
+const request = require("request");
 const episode = process.argv[2];
+const characters = [];
 
 request(
-  'https://swapi-api.hbtn.io/api/films/' + episode,
+  "https://swapi-api.hbtn.io/api/films/" + episode,
   (error, response, body) => {
     if (error) {
       console.log(error);
     } else {
       const json = JSON.parse(body);
-      json.characters.forEach((character) => {
-        request(character, (error, response, body) => {
-          if (error) {
-            console.log(error);
-          } else {
-            const json = JSON.parse(body);
-            console.log(json.name);
-          }
-        });
-      });
+
+      listCharacters(json.characters);
     }
+    // json.characters.forEach((characterAPI) => {
+    // });
+
+    // console.log(characters);
   }
 );
+
+const listCharacters = (characterAPIList) => {
+  if (characterAPIList.length !== 0) {
+    request(characterAPIList[0], (error, response, body) => {
+      if (error) {
+        console.log(error);
+      } else {
+        const json = JSON.parse(body);
+        console.log(json.name);
+
+        listCharacters(characterAPIList.slice(1));
+
+        // return JSON.parse(body).name;
+      }
+    });
+  }
+};
