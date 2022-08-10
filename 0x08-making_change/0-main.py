73e3e082b334b3c56fdab2ff4820d594f614160a
Noah Chernet <noahchernet@yahoo.com>
2022-08-10 17:34:01 +0300

    Added more tests for the function

--- 0x08-making_change/0-main.py
+++ 0x08-making_change/0-main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """
 Main file for testing
 """
@@ -5,5 +6,14 @@ Main file for testing
 makeChange = __import__('0-making_change').makeChange
 
 print(makeChange([1, 2, 25], 37))
-
+print('---')
 print(makeChange([1256, 54, 48, 16, 102], 1453))
+print('---')
+print(makeChange([1, 2, 3, 4, 25], 100))
+print('---')
+print(makeChange([1, 2, 3, 4, 25], 76))
+print('---')
+print(makeChange([1, 2, 3, 4, 25], 101))
+print('---')
+print(makeChange([1, 2, 3, 4, 25], 105))
+
